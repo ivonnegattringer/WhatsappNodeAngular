c35d467a1fa604f12e294604f466917c1c11f789
ivonnegattringer <ivonne.gattringer@gmail.com>
2020-01-21 13:24:44 +0100

    lkajsdf

--- Server/websocket.ts
+++ Server/websocket.ts
@@ -2,36 +2,31 @@ import * as WebSocket from 'ws';
 import * as Http from 'http';
 //https://github.com/websockets/ws/tree/master/examples/express-session-parse
 
-const port = 3000;
+const port = 8000;
 const server = Http.createServer();
 const wss = new WebSocket.Server({noServer: true});
+
 
 console.log('Server listening on port ' + port);
 
 wss.on('connection', function connection(ws, request, client) {
-    ws.on('message', function message(msg) {
-      console.log(`Received message ${msg} from user ${client}`);
-      boradcast(msg);
-    });
-  });
-  
-server.on('upgrade', function upgrade(request, socket, head) {
-    authenticate(request, (err, client) => {
-      if (err || !client) {
-        socket.destroy();
-        return;
+    ws.on('message', data=> {
+      let message = JSON.parse(data);
+      switch(message.type){
+        case 'data':
+          ws.send({type: 'data', reply: 'i got a reakin message u stupid kiddo'});
+          break;
+        default:
+          ws.send('yikes');
+          break;
       }
-  
-      wss.handleUpgrade(request, socket, head, function done(ws) {
-        wss.emit('connection', ws, request, client);
-      });
     });
   });
 
-function boradcast (data: string){
+/*function boradcast (data: string){
     wss.clients.forEach(client => { 
         if(client.readyState === WebSocket.OPEN) client.send(data);
     });
-}
+}*/
 
 server.listen(port);
