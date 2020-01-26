f255200e2d4ca287874a3d5af1a7bb7196bebaa2
Kommenda <kommendamanuel@gmail.com>
2020-01-26 20:13:12 +0100

    server

--- Server/server.ts
+++ Server/server.ts
@@ -3,26 +3,29 @@ import { Repository } from './repository/respository';
 import { User } from './entity/user';
 
 var server = express();
+var repo : Repository;
 const port = 2000;
 
 server.use(express.static('public'));
 server.use(express.json());
-
 
 server.get('/api/echo', (request, response) => {
     response.send("Success");
 });
 
 server.get('/get/user/', async  (request, response) => {
-
+    console.log('Get Users');
     var username = request.query.username;
     var password = request.query.password;
 
-    var repo = new Repository;
+    if(username == '' || password == ''){
+        response.send('false');
+    }
+    
     var user = repo.findAllUsers().then(data => {
-        data.find(u => u.username == username && u.password == password)
+        return data.find(u => u.username == username && u.password == password)
     });
-
+    console.log(user);
     if(user == null){
         response.send('false');
     }
@@ -32,5 +35,6 @@ server.get('/get/user/', async  (request, response) => {
 
 
 server.listen(port, function(){
-    console.log(`API is listening on port ${port}`)
+    console.log(`API is listening on port ${port}`);
+    repo = new Repository();
 });
